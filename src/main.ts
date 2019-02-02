7435ffebdd238da157356522f3fd5c9577ec1f07
Ivo Spijkerman <ivo@spijkerman.nl>
2019-02-02 02:24:18 +0100

    added new miner/controller role

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,35 @@
 import { ErrorMapper } from "utils/ErrorMapper";
+import { RoleHarvester } from "roles/harvester";
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
+
+  /*
+  * The code below handles the spawning of Creeps
+  */
+  const populationSize = Object.keys(Game.creeps).length // TODO
+  const desiredPopulationSize = 5;
+
+  if (populationSize < desiredPopulationSize) {
+    // This only works if the player only has one spawn / one room
+    // TODO add multiple room support
+    // TODO different populationSizes per room according tot their tech level?
+    const spawn = Object.values(Game.spawns)[0];
+    // TODO support multiple roles and different levels, according to room tech level
+    // body found via https://codepen.io/findoff/details/RPmqOd
+    spawn.createCreep([MOVE, MOVE, CARRY, WORK]);
+  }
+
+  /*
+  * The code below handles the mining behaviour of creeps
+  */
+  for (const name in Game.creeps) {
+    const creep = Game.creeps[name];
+    RoleHarvester.run(creep);
+  }
+
 
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
---
+++ src/roles/harvester.ts
@@ -0,0 +1,28 @@
+export class RoleHarvester {
+    public static run(creep: Creep) {
+        if (creep.carry.energy < creep.carryCapacity) {
+            // This selects a random source
+            // May not be the closest one
+            // May change even during mining
+            const source = creep.room.find(FIND_SOURCES)[0];
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
+        } else {
+            const spawn = creep.room.find(FIND_MY_SPAWNS)[0];
+            // Check if the spawn needs some energy, if so, refuel spawn
+            // Otherwise rebuild the controller
+            if (spawn.energy < spawn.energyCapacity) {
+                if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(spawn);
+                }
+            } else {
+                const controller = creep.room.controller;
+                // Make sure that the room has a controller (needed for TS)
+                if (controller != undefined && creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(controller);
+                }
+            }
+        }
+    }
+};
